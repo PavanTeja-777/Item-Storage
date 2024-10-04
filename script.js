f404b592f057c6a88a362b11001c348d7c12dd4d
Pavan Teja Sheela <psptsonofsantosh@gmail.com>
2024-10-04 08:27:10 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-if (!localStorage.getItem(0)) {
-    localStorage.setItem(0,1)
-    localStorage.setItem(1,JSON.stringify([]))
+if (!localStorage.getItem(1000)) {
+    localStorage.setItem(1000,1)
+    localStorage.setItem(1001,JSON.stringify([]))
 }
 
 const add_func = () => {
-    let index = parseInt(localStorage.getItem(0))   
+    let index = parseInt(localStorage.getItem(1000))   
     let item_name = document.querySelector('#item-name-input').value
     let item_qty = document.querySelector('#item-qty-input').value
 
-    let data = JSON.parse(localStorage.getItem(1))
+    let data = JSON.parse(localStorage.getItem(1001))
     data.push([item_name,item_qty])
-    localStorage.setItem(1, JSON.stringify(data))
+    localStorage.setItem(1001, JSON.stringify(data))
 
-    localStorage.setItem(0, index + 1)
+    localStorage.setItem(1000, index + 1)
     let table = document.querySelector("table")
     let row = table.insertRow(index)
     row.insertCell().textContent = item_name
@@ -24,7 +24,8 @@ const add_func = () => {
 
 const update = () => {
     let table = document.querySelector("tbody")
-    let data = JSON.parse(localStorage.getItem(1))
+    let data = JSON.parse(localStorage.getItem(1001))
+
     for (let index = 0; index < data.length; index++) {
         const element = data[index];
         let row = table.insertRow(index)
@@ -34,8 +35,9 @@ const update = () => {
 }
 
 const clear_data = () => {
-    localStorage.setItem(0,1)
-    localStorage.setItem(1,JSON.stringify([]))
+    localStorage.setItem(1000,1)
+    localStorage.setItem(1001,JSON.stringify([]))
+    window.location.reload()
 }
 
 window.onload = () => {
